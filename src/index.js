1c7251fa666737ea40b097ad11d4c52233cdc502
Kirill Tukach <kirill.tukach@gmail.com>
2024-10-14 21:11:11 +0300

    feat: add function checked brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+    const openBrackets = bracketsConfig.map(pair => pair[0]);
+    const closeBrackets = bracketsConfig.map(pair => pair[1]);
+    const bracketsMap = Object.fromEntries(bracketsConfig);
+
+    for (let char of str) {
+        if (openBrackets.includes(char)) {
+            if (closeBrackets.includes(char) && stack[stack.length - 1] === char) {
+                stack.pop();
+            } else {
+                stack.push(char);
+            }
+        } else if (closeBrackets.includes(char)) {
+            if (stack.length === 0 || bracketsMap[stack.pop()] !== char) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
 }
